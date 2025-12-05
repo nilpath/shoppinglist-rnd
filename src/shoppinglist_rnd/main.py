1a32c686e17fbe1c0bf8e61cc7d19c35cd2a3d73
Christoffer Åhrling <christoffer@nilpath.se>
2025-12-05 12:01:04 +0100

    add mlflow

--- src/shoppinglist_rnd/main.py
+++ src/shoppinglist_rnd/main.py
@@ -2,11 +2,18 @@ import os
 
 import dspy
 import dotenv
+import mlflow
 
 from shoppinglist_rnd.agent import ShoppingListAgent
 from shoppinglist_rnd.models import Ingredient
 
 dotenv.load_dotenv('.env')
+
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+mlflow.set_experiment(os.getenv("MLFLOW_EXPERIMENT_NAME"))
+
+mlflow.dspy.autolog()
+
 
 if __name__ == "__main__":
 
@@ -15,8 +22,8 @@ if __name__ == "__main__":
 
     ingredients = [
         Ingredient(name="Äpplen (Royal Gala)", quantity=2, unit="pcs"),
-        Ingredient(name="Mjölk", quantity=1, unit="liter", notes="1.5%, läng hållbarhet"),
-        Ingredient(name="Fryst Kycklingbröst", quantity=500, unit="grams"),
+        Ingredient(name="Mjölk", quantity=1, unit="liter", notes="1.5%, lång hållbarhet"),
+        Ingredient(name="Fryst Kycklingbröstfilé", quantity=500, unit="grams"),
         Ingredient(name="Frysta ärtor", quantity=300, unit="grams"),
     ]
 
